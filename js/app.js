dc9ffb9c836d7352a7dd0553a52861a1c3c20356
lgsmd <584588192@qq.com>
2019-06-18 18:31:08 +0800

    add remind for pass game

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const carArray = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
 const DocFrag = document.createDocumentFragment();
-const platformElement = document.querySelector('.deck');
+const gamePlatfrom = document.querySelector('.deck');
 const starsMode = document.querySelectorAll(".fa.fa-star");
+const resetButton = document.querySelector('.restart');
+const eventCard = document.querySelector('.deck');
+const divContainer = document.querySelector(".container");
+const scoreString = document.getElementById("scoreString");
+const passPlatfrom = document.querySelector(".passPlatfrom");
+const playAgain = document.getElementById("playAgain");
+
 let matchArray = [];
 let moves = 0;
+let passTiming = 0;
 
 makePlatform(shuffle(carArray));
 
@@ -19,7 +27,7 @@ function shuffle(array) {    //洗牌函数
     }
     return array;
 }
-function movesTiming(timing){        //计星，计次
+function movesTiming(timing){        //计星
     document.querySelector(".moves").textContent = timing;
     if(timing === 15){
         starsMode[2].className = "fa fa-star-o";
@@ -39,7 +47,7 @@ function makePlatform(carArray) {  //生成游戏表格
         newLi.appendChild(newI);
         DocFrag.appendChild(newLi);
     }
-    platformElement.appendChild(DocFrag);
+    gamePlatfrom.appendChild(DocFrag);
 }
 
 function checkCards(className1, className2){   //对比卡片
@@ -48,10 +56,12 @@ function checkCards(className1, className2){   //对比卡片
         openCard[0].className = "card match";
         openCard[1].className = "card match";
         setTimeout(function () {
-
+            passTiming++;
+            if(passTiming === 8){     //正确匹配8次，通关游戏
+                passGame(passTiming);
+            }
             matchArray = [];
         }, 550);
-        moves += 1;
     }else {
         openCard[0].className = "card notRight";
         openCard[1].className = "card notRight";
@@ -60,26 +70,42 @@ function checkCards(className1, className2){   //对比卡片
             openCard[1].className = "card";
             matchArray = [];
         }, 550);
-        moves += 1;
     }
     movesTiming(moves);
 }
 
-document.querySelector('.restart').addEventListener("click", function() { //触发重置
-    while (platformElement.firstChild != null){
-        platformElement.firstChild.remove();
+function resetClick(){  // 重置游戏
+    while (gamePlatfrom.firstChild != null){
+        gamePlatfrom.firstChild.remove();
     }
     moves = 0;
     movesTiming(moves);
     makePlatform(shuffle(carArray));
-});
+}
 
-document.querySelector('.deck').addEventListener('click', function (event) { //点击卡片
+function againPlay() {
+    resetClick();
+    passTiming = 0;
+    passPlatfrom.style.cssText = "visibility: hidden; height: 0px";
+    divContainer.style.cssText = "visibility: visible";
+}
+
+function passGame(){    //通关游戏
+    divContainer.style.cssText = "visibility: hidden; height: 0px";  // 隐藏游戏界面
+    scoreString.textContent = `With ${moves} Moves and ${document.querySelectorAll(".fa.fa-star").length} Stars.`;
+    passPlatfrom.style.cssText = "visibility: visible; height: 100%"; // 显示通过恭喜界面
+    playAgain.addEventListener('click', againPlay);
+}
+
+resetButton.addEventListener('click', resetClick); //触发重置
+
+eventCard.addEventListener('click', function (event) { //点击卡片
     if(event.target.className === "card") {
         if(matchArray.length === 0) {
             event.target.className = "card open show";
             matchArray[0] = event.target.firstElementChild.className;
         }else if(matchArray.length === 1){
+            moves += 1;
             event.target.className = "card open show";
             matchArray[1] = event.target.firstElementChild.className;
             checkCards(matchArray[0],matchArray[1]);
